42fffc09e37cfa54a6e177bc899e2f68cd041228
MuzammilPIAIC <50516983+MuzammilPIAIC@users.noreply.github.com>
2022-12-14 22:01:23 +0500

    Update urls.py

--- voice_record/urls.py
+++ voice_record/urls.py
@@ -2,8 +2,8 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('',        home,   name = 'home'),
-    path('record/',  record, name = 'record'),
+#     path('',        home,   name = 'home'),
+    path('',  record, name = 'record'),
     path('new_record_/',  new_record_, name = 'new_record_'),
     path('record/details/<int:id>',  record_detail, name = 'details'),
     path("record/text/<str:id>/", text, name="text"),
