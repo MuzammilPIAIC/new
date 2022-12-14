df4b7b4cdbc9446f6988b334481d6b99d8e7b356
MuzammilPIAIC <50516983+MuzammilPIAIC@users.noreply.github.com>
2022-12-14 21:42:50 +0500

    Update admin.py

--- voice_record/admin.py
+++ voice_record/admin.py
@@ -1,2 +1,24 @@
 from django.contrib import admin
 
+
+from voice_record.models import Record, Question, Step
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ("id", "language", "voice_record")
+
+
+class Question_details(admin.ModelAdmin):
+    list_display    = ('question','display_question_on_page')
+    list_filter     = ('display_question_on_page',)   
+    search_fields   = ['question']
+
+
+class Step_detail(admin.ModelAdmin):
+    list_display    = ('page_number','duration_in_sec','no_question_to_display')
+    search_fields   = ['page_number']
+    list_filter     = ('page_number',)
+
+admin.site.register(Question,Question_details)
+admin.site.register(Step,Step_detail)
